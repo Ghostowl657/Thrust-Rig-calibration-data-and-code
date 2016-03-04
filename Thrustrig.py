438f79266149a384143151f6f20e1b453b19278d
Brandon Nadal <bn239@cornell.edu>
2016-03-03 22:13:57 -0500

    Added files via upload

--- Thrustrig.py
+++ Thrustrig.py
@@ -1,10 +1,7 @@
 # Thrustrig.py
-# Brandon Nadal
-# 2/29/16
+# Brandon Nadal, (add your name if you work on this)
 
-"""Takes data points from the thrust rig and calculates, using least
- squares regression analysis, and calculates the function [V=a+b*W] or
- [V=a+bln(W)]"""
+"""Does many things related to the Thrust rig. See the README."""
 
 # Uses formulas from Wolfram Mathworld
 # <http://mathworld.wolfram.com/LeastSquaresFitting.html>
@@ -15,83 +12,118 @@
 # Equations (3) and (4)
 
 from math import log,sqrt,e
-from time import time,sleep
+import matplotlib.pyplot as plt # you need to install this module from command shell
+from time import time,clock,ctime
+import serial # the serial module needs to be downloaded
+import sys # this might only work on Windows
 
-# Weight of test 1
-W1 = [2.024,2.516,3.084,3.64,4.36,4.918,5.616,6.25,
-      6.972,7.694,8.234,8.874] # removed 0.254,0.56,0.884,1.188,1.532 outliers
-# Voltage of test 1
-V1 = [4.0,4.2,4.33,4.47,4.57,4.62,4.69,4.74,4.78,
-      4.82,4.85,4.87] # removed 1.9,2.49,2.79,3.32,3.8 outliers
-# Weight of test 2
-W2 = [1.532,2.024,2.516,3.214,3.934,4.656,5.376,5.944,6.484,
-      7.042,7.598,8.238,8.872] # removed 0.254 outlier
-# Voltage of test 2
-V2 = [3.9,4.07,4.22,4.36,4.49,4.6,4.67,4.72,4.78,
-      4.8,4.81,4.83,4.86] # removed 1.9 outlier
-# Weight of test 3
-W3 = [6.292,6.78,7.272,7.992,8.712,9.114,9.416,10.008,
-      10.384,10.704,11.024] # removed 1.28 outlier
-# Voltage of test 3
-V3 = [4.77,4.79,4.82,4.85,4.88,4.92,4.93,4.93,4.94,
-      4.94,4.95] # removed 3.635 outlier
-# Weight of test 4
-W4 = [1.28,6.292,6.612,7.206,7.926,8.33,8.65,9.37,9.862,
-      10.35,10.654,10.97]
-# Voltage of test 4
-V4 = [4.22,4.775,4.8,4.825,4.85,4.87,4.89,4.905,4.92,
-      4.935,4.94,4.95]
-# Weight of test 5
-W5 = [1.28,6.292,7.012,7.732,8.048,8.352,8.672,8.992,9.396,
-      9.888,10.376,10.97]
-# Voltage of test 5
-V5 = [4.22,4.775,4.8,4.85,4.86,4.87,4.88,4.89,4.9,4.91,
-      4.925,4.945]
 
 def isfloat(value):
-  """Determines if a value is a float
-  Output is Boolean
-  """
-  try:
-    float(value)
-    return True
-  except ValueError:
-    return False
+    """Determines if a value is a float
+    Output is Boolean
+    """
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
-"""# Getting Voltage and Weight lists from a data text file
-fo = open('Thrustdata.txt', 'r')
-V = []
-W = []
-n = 0
-position = 0
-val = 0
-val_meaning = 'Number'
-fo.seek(0, 2)
-length = fo.tell()
-fo.seek(0, 0)
-while n != length:
-    line = fo.readline();
-    if line == 'V\n':
-        val_meaning = 'Voltage'
-    elif line == 'W\n':
-        val_meaning = 'Weight'
-    elif isfloat(line):
-        val = float(line)
-        if val_meaning == 'Voltage':
-            V = V + [val]
-        elif val_meaning == 'Weight':
-            W = W + [val]
-    n = n + 1
-fo.close()"""
 
-# Concatenating
-W = W1+W2+W3+W4+W5
-V = V1+V2+V3+V4+V5
+def islist(value):
+    """[WIP] Determines if a value is a list
+    Output is Boolean
+    """
+    try:
+        value.insert(0,16)
+        return True
+    except ValueError:
+        return False
+
+
+def getVWData():
+    """Gets Voltage and Weight lists from Thrustdata.txt
+    """
+    fo = open('Thrustdata.txt', 'r')
+    V = []
+    W = []
+    n = 0
+    val = 0
+    val_meaning = 'Number'
+    # go to the end of the file, record how long it is, then go back to the beginning
+    fo.seek(0, 2)
+    length = fo.tell()
+    fo.seek(0, 0)
+    # go through file line by line
+    while n != length:
+        line = fo.readline(); # reads a line of the file and stores it
+        if line == 'V\n':
+            val_meaning = 'Voltage'
+        elif line == 'W\n':
+            val_meaning = 'Weight'
+        elif isfloat(line):
+            val = float(line)
+            if val_meaning == 'Voltage':
+                V = V + [val]
+            elif val_meaning == 'Weight':
+                W = W + [val]
+        n = n + 1
+    fo.close()
+    return (V,W)
+
+
+def getVoltageData():
+    """Stores data in VoltageData.txt as a list.
+    See getVWData() for comments.
+    """
+    fo = open('VoltageData.txt', 'r')
+    Vmes = []
+    n = 0
+    fo.seek(0, 2)
+    length = fo.tell()
+    fo.seek(0, 0)
+    while n != length:
+        line = fo.readline(6);
+        if isfloat(line) and len(str(line))==6:
+            val = float(line)
+            Vmes = Vmes + [val]
+        n = n + 1
+    fo.close()
+    return Vmes
+  
+  
+def getCapacityData():
+    """Takes data in MotorCurrent.txt and MotorVoltage.txt and stores as lists.
+    See getVWData() for comments.
+    """
+    fI = open('MotorCurrent.txt', 'r')
+    fV = open('MotorVoltage.txt', 'r')
+    I = []
+    V = []
+    n = 0
+    fI.seek(0, 2)
+    lengthI = fo.tell()
+    fI.seek(0, 0)
+    fV.seek(0, 2)
+    lengthV = fo.tell()
+    fV.seek(0, 0)
+    while n != lengthI:
+        lineI = fI.readline();
+        if isfloat(lineI):
+            I = I + [float(lineI)]
+    while n != lengthV:
+        lineV = fV.readline();
+        if isfloat(lineV):
+            V = V + [float(lineV)]
+        n = n + 1
+    fI.close()
+    fV.close()
+    return (I,V)
 
 
 def sum_sqrd(x,function='lin'):
     """Finds the sum of x**2 or (lnx)**2.
-    x is a list. function is either 'log' or empty.
+    
+    PreC: x is a list. function is either 'log' or empty.
     """
     x_sum_list = []
     for c in range(len(x)):
@@ -104,7 +136,9 @@ def sum_sqrd(x,function='lin'):
 
 
 def sum_ln(x):
-    """Finds the sum of ln(x). x is a list.
+    """Finds the sum of ln(x).
+    
+    PreC: x is a list.
     """
     lnx = []
     for n in range(len(x)):
@@ -118,7 +152,8 @@ def sum_ln(x):
 
 def sum_xy(x,y,function='lin'):
     """Finds the value of a sum of x*y or yln(x).
-    x and y are lists. function is either 'log' or empty.
+    
+    PreC: x and y are lists. function is either 'log' or empty.
     """
     product = []
     if len(x) <= len(y):
@@ -136,7 +171,8 @@ def sum_xy(x,y,function='lin'):
 
 def find_ab(x,y,function):
     """Uses wolfram's equations to calculate a and b.
-    x and y are lists, function is 'linear', 'logarithmic', or 'exponential'.
+    
+    PreC: x and y are lists, function is 'linear', 'logarithmic', or 'exponential'.
     """
     if function == 'logarithmic':
         xtemp = x
@@ -147,9 +183,10 @@ def find_ab(x,y,function):
     y_bar = sum(y)/len(y)
     x_sqr = sum_sqrd(x)
     xy_sum = sum_xy(x,y)
-    ylnx_sum = sum_xy(x,y,'log')
-    xlny_sum = sum_xy(y,x,'log')
-    lnx_sqr = sum_sqrd(x,'log')
+    if function!='linear':
+        ylnx_sum = sum_xy(x,y,'log')
+        xlny_sum = sum_xy(y,x,'log')
+        lnx_sqr = sum_sqrd(x,'log')
     
     if len(x) <= len(y):
         n = float(len(x))
@@ -168,11 +205,42 @@ def find_ab(x,y,function):
     return (a,b)
 
 
-def test_ab(a,b,x,y,function,d=0):
+def MakeWindow(minx,maxx,miny,maxy,labels=True,bgcolor=[1.0,1.0,1.0]):
+    """Creates a window with x range minx<=x<=maxx and y range miny<=y<=maxy
+
+    If labels is set to False, it will turn off the labeled axes.
+    Labeling will not look good if the range is too large, e.g., range>10.
+    
+    The window will have a background color specified by bgcolor.
+    The default is white.
+    
+    PreC: minx,maxx,miny,maxy are numbers. labels is boolean.
+    bgcolor is an RGB list.
+    """
+    plt.figure(figsize=(8,8), dpi=80)
+    # Where to put the axis ticks.
+    plt.xticks(np.linspace(int(minx), int(maxx), int(maxx-minx)+1, endpoint=True))
+    plt.yticks(np.linspace(int(minx), int(maxy), int(maxy-miny)+1, endpoint=True))
+    # The x and y ranges along the axes.
+    plt.xlim(int(minx),int(maxx))
+    plt.ylim(int(miny),int(maxy))
+    # Background color
+    axes = plt.gca() #get current axes
+    axes.set_axis_bgcolor(bgcolor) 
+    if not labels:
+        # Suppress the ticks
+        axes.set_xticks([]) # remove number labels and ticks
+        axes.set_yticks([])
+
+
+def stats(a,b,x,y,function):
     """Takes values for a and b and calculates the errors.
-    a and b are numbers, x and y are lists, function is 'linear', 'logarithmic', or 'exponential'.
+    
+    PreC: a and b are numbers, x and y are lists, function is 'linear',
+    'logarithmic', or 'exponential'.
     """
     error = []
+    residual = []
     if len(x) <= len(y):
         n = len(x)
     else:
@@ -181,24 +249,40 @@ def test_ab(a,b,x,y,function,d=0):
         if function == 'linear':
             y_calc = a + b*x[c]
             error = error + [abs((y[c]-y_calc)/y[c])]
+            residual = residual + [y[c]-y_calc]
         elif function == 'logarithmic':
             x_calc = a + b*log(y[c])
             error = error + [abs((x[c]-x_calc)/x[c])]
+            residual = residual + [x[c]-x_calc]
         elif function == 'exponential':
-            y_calc = a*e**(b*x[c])+d
+            y_calc = a*e**(b*x[c])
             error = error + [abs((y[c]-y_calc)/y[c])]
+            residual = residual + [y[c]-y_calc]
     av_err = sum(error)/len(error)
-    return (av_err,error)
+    # the following lines are commented out, but plot the residuals of the approximate function
+    #
+    # if function == 'logarithmic':
+    #   x1 = residual
+    #   residual = x
+    #   x = x1
+    # MakeWindow(min(x)-2,max(x)+2,min(y)-2,max(y)+2)
+    # plt.scatter(x,y)
+    # MakeWindow(min(x)-2,max(x)+2,min(residual)-2,max(residual)+2)
+    # plt.plot([0,7],[0,0],linewidth=1,color=[0,0,0])
+    # plt.scatter(x,residual)
+    # plt.show()
+    return (av_err,error,residual)
 
 
 def PrintStuff(function,V,W):
     """Finds a and b from V and W, tests it, then prints the equation and errors.
-    V, W are lists. function is 'linear', 'logarithmic', or 'exponential'
+    
+    PreC: V, W are lists. function is 'linear', 'logarithmic', or 'exponential'
     """
     h = find_ab(V,W,function)
     a = h[0]
     b = h[1]
-    g = test_ab(a,b,V,W,function)
+    g = stats(a,b,V,W,function)
     av_err = g[0]*100
     errormax = max(g[1])
     errormin = min(g[1])
@@ -213,72 +297,125 @@ def PrintStuff(function,V,W):
     print '%18s %1.5f %0s' % ('Average Error =',av_err,'%')
     print '%14s %1.3f %0s %1.3f %0s' % ('Max error =',errormax*100,
                                     '%, Min error =',errormin*100,'%')
-    return g
+    return (g,a,b)
+
+
+def capacity(V,I,del_t):
+    """[WIP] Takes Voltage, Current, and time interval to
+    calculate Power and Energy.
+    
+    PreC: V,I are both lists or numbers. del_t is a number or list.
+    """
+    power = []
+    total_capacity = []
+    for n in range(len(V)):
+        power = power + [V[n]*I[n]]
+    total_capacity = sum_xy(power,del_t)
+    print (total_capacity,power)
 
 
 if __name__ == '__main__':
-    choice = str(raw_input(' Linear, Logarithmic, or Exponentional approximation?: '))
-    rang = input(' Weight range? (True/False): ')
-    if rang:
-        lowW = input(' Low W: ')
-        highW = input(' High W: ')
-        r = len(W)
-        W0 = W
-        V0 = V
-        W = []
-        V = []
-        n = 0
-        while n < len(W0):
-            if W0[n] >= lowW and W0[n] <= highW: # making new lists for W and V with all lists within the range
-                W = W + [W0[n]]
-                V = V + [V0[n]]
-            n = n + 1
-    if ('lin' in choice or 'Lin' in choice):
-        function = 'linear'
-        go = True
-    elif ('log' in choice or 'Log' in choice or choice == 'ln'):
-        function = 'logarithmic'
-        go = True
-    elif ('exp' in choice or 'Exp' in choice or choice == 'e'):
-        function = 'exponential'
-        go = True
-    else:
-        print 'Sorry bad input, try again.'
-        go = False
-    if go:
-        g = PrintStuff(function,V,W)
-        print '   Data =',len(V)
-        # print g[1]
-        
-        """delt_err = -1
-        d = 0
-        err_mem = []
-        while delt_err < 0 and function == 'exponential':
-            st_err = g[1]
-            d = d - 10**(-6)
-            h = find_ab(V,W,function)
-            g = test_ab(h[0],h[1],V,W,function,d)
-            en_err = g[1]
-            delt_err = sum(en_err)/len(en_err)-sum(st_err)/len(st_err)
-            err_mem = err_mem + [delt_err]
+    while True: # program will continue to loop until you press CTRL+C
+        BAD = False
+        takedata = input(' Take data?: ')
+        if takedata:
+            reps_or_naw = raw_input(' Timer, Keypress end, or Number of Data? (T, K, or N): ')
+            with serial.Serial('COM4',9600) as port, open('VoltageData.txt','a') as outf:
+                if reps_or_naw == 'T':
+                    interval = input(' How long in seconds: ')
+                    begindotT = clock()
+                    elapsedT = 0
+                    while elapsedT <= interval:
+                        dotT = clock() - begindotT
+                        if dotT >= 0.5: # this makes a loading bar of sorts
+                            dotdot = dotdot + '.'
+                            sys.stdout.write('\r'+dotdot) # sys might be Windows only
+                            if len(dotdot) >= 35:
+                                dotdot = ' Gathering Data.'
+                                sys.stdout.write('\r'+' Gathering Data.'+' '*25)
+                            begindotT = clock()
+                        x = port.read(size=6) # reads a line (e.g. 0.1932)
+                        outf.writelines(x) # and writes it to VoltageData.txt
+                        endT = clock()
+                        elapsedT = endT - beginT
+                elif reps_or_naw == 'N':
+                    reps = input(' How many data?: ')
+                    init = port.read(size=8)
+                    for n in range(reps):
+                        x = port.read(size=6)
+                        outf.writelines(x)
+                elif reps_or_naw == 'K':
+                    cont = True
+                    beginT = clock() - 1.5
+                    print ' '
+                    print ' Press any key to stop' # this doesn't happen yet...
+                    print ' '
+                    dotdot = ' Gathering Data'
+                    while cont:
+                        interval = clock() - beginT
+                        if interval >= 0.5: # this makes a loading bar of sorts
+                            dotdot = dotdot + '.'
+                            sys.stdout.write('\r'+dotdot) # see note above about sys
+                            if len(dotdot) >= 35:
+                                dotdot = ' Gathering Data.'
+                                sys.stdout.write('\r'+' Gathering Data.'+' '*25)
+                            beginT = clock()
+                        x = port.read(size=6) # reads a line (e.g. 0.1932)
+                        outf.writelines(x) # and writes it to VoltageData.txt
+                sleep(0.25)
+                outf.close()
+        elif not takedata:
+            whatdo = raw_input(' Force, Capacity: ')
+            if whatdo == 'Force': # Using force sensor
+                calib = input(' Calibration?: ')
+                # Finding the function of force to voltage through least squares approximation
+                if calib:
+                    choice = raw_input(' Linear, Logarithmic, or Exponentional approximation?: ')
+                    j = getVWData()
+                    V = j[0]
+                    W = j[1]
+                    go = True
+                    if ('lin' in choice or 'Lin' in choice):
+                        function = 'linear'
+                    elif ('log' in choice or 'Log' in choice or choice == 'ln' or choice == 'Ln'):
+                        function = 'logarithmic'
+                    elif ('exp' in choice or 'Exp' in choice or choice == 'e' or choice == 'E'):
+                        function = 'exponential'
+                    else:
+                        go = False
+                        BAD = True
+                    if go:
+                        rang = input(' Weight range? (True/False): ')
+                        if rang:
+                            lowW = input(' Low W: ')
+                            highW = input(' High W: ')
+                            r = len(W)
+                            W0 = W # temporary storage
+                            V0 = V # ""             ""
+                            W = []
+                            V = []
+                            n = 0
+                            while n < len(W0):
+                                # making new lists for W and V with all lists within the range
+                                if W0[n] >= lowW and W0[n] <= highW:
+                                    W = W + [W0[n]]
+                                    V = V + [V0[n]]
+                                n = n + 1
+                        g = PrintStuff(function,V,W)
+                        print '   Data =',len(V) # number of data point used in regression
+                elif not calib:
+                    # input Voltage data and do stuff with it. Like output force, graph, etc.
+                    pass
+            elif whatdo == 'Capacity':
+                j = getCapacityStuff()
+                I = j[0]
+                V = j[1]
+                pass
+            else:
+                BAD = True
         else:
-            if function == 'exponential':
-                err_mem = sum(err_mem)/len(err_mem)
-                print d, delt_err, err_mem
-                PrintStuff(function,V,W)"""
-        
-        """redo = input('Make it more accurate?: ')
-        while redo == True:
-            accuracy = input('How accurate?: ')
-            Vacc = []
-            Wacc = []
-            err = g[1]
-            for n in range(len(V)):
-                if err[n] < accuracy:
-                    Vacc = Vacc + [V[n]]
-                    Wacc = Wacc + [W[n]]
-            if len(Vacc) == 0:
-                quit()
-            PrintStuff(function,Vacc,Wacc)
-            print '   Data=',len(Vacc)
-            redo = input('Make it more accurate?: ')"""
+            BAD = True
+        if BAD:
+            print ' Sorry, bad input. Try again.'
+        else:
+            print ' Here we go again!'
